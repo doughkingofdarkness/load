87ba43065147a9e6a36fbe71d16ef34da0e8c9f5
kaisson <redhatsad1@gmail.com>
2025-02-15 18:02:55 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -3,19 +3,16 @@ app = Flask(__name__)
 ugs = [
     "FloraAgent"
 ]
+def safe(user,script):
+    if ug in ugs:
+        return jsonify(script)
+    else:
+        return jsonify("game.Players.LocalPlayer:Kick('Your Executor is not supported! Join Colonel Server and contact the Owner')")
 @app.route('/')
 def hme():
     return jsonify("Running!")
 @app.route('/loader/colonel')
 def home():
     script = 'print("hello world")'
-    found = False
     ug = request.headers.get('user-agent')
-    for i,v in range(ugs):
-        if v == ug:
-            found = True
-            break 
-    if found == True:
-        return jsonify(script)
-    else:
-        return jsonify("game.Players.LocalPlayer:Kick('Your Executor is not supported! Join Colonel Server and contact the Owner')")
+    return safe(ug,script)
