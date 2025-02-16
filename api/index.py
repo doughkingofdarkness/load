dc02177568f3ffdb712c445928f510fa2e2b5ee2
kaisson <redhatsad1@gmail.com>
2025-02-16 13:35:28 +0200

    Update index.py

--- api/index.py
+++ api/index.py
@@ -2,6 +2,13 @@ from flask import Flask,request
 
 app = Flask(__name__)
 ugs = ["FloraAgent"]
+def readfile(path):
+    try:
+        with open(path, "r") as f: 
+            content = f.read()
+            return content
+    except FileNotFoundError:
+        return None
 def safe(user,script):
     if user in ugs:
         return script
@@ -9,7 +16,7 @@ def safe(user,script):
         return "game.Players.LocalPlayer:Kick('Your Executor is not supported! Join Colonel Server and contact the Owner')"
 @app.route('/loader/colonel')
 def home():
-    script = 'print("hello world")'
+    script = readfile("scripts/loader-colonel")
     ug = request.headers.get('User-Agent')
     return safe(ug, script)
 @app.route('/')
